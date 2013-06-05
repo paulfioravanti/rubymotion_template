ccd9ccc528a180cbd6ccb28be0bea02c0faf07e8
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-05 18:41:36 +0930

    Add spec_helper-style file for methods defined on UIWindow objects used in specs

--- files/app/application_delegate.rb
+++ files/app/application_delegate.rb
@@ -4,6 +4,7 @@ class ApplicationDelegate
   # Replace root_view_controller with your own
   # Rubymotion displays warnings if it is not set
   def initialize
+    # return true if RUBYMOTION_ENV == 'test'
     @window = ApplicationWindow.new
     @window.root_view_controller = ExampleController.new
     @window.make_key_and_visible
--- files/app/helpers/app_delegate_helper.rb
+++ files/app/helpers/app_delegate_helper.rb
@@ -1,6 +1,5 @@
 module AppDelegateHelper
   def application(application, didFinishLaunchingWithOptions: launchOptions)
-    # return true if RUBYMOTION_ENV == 'test'
     initialize
     true
   end
