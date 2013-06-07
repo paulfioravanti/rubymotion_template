8e8c71efffc1a246856819b74bb0f8bce90aa392
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-07 22:38:53 +0930

    Not possible to define dependencies in spec files yet, so they get renamed.  Also, add in pretty colours to test output

--- files/spec/00_spec_helper.rb
+++ files/spec/00_spec_helper.rb
@@ -2,8 +2,6 @@ module SpecHelper
   def self.extended(base)
     # This is specifically for the UIWindow returned by
     # application_instance (sharedApplication) in the specs
-    UIWindow.send(:define_method, :root_view_controller) do
-      self.rootViewController
-    end
+    UIWindow.send :alias_method, :root_view_controller, :rootViewController
   end
 end
