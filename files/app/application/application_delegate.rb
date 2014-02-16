4e797209b1e2a0bfc822e3ad63f6d48942f8a427
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-02-16 23:01:23 +1100

    Move ApplicationWindow setting properties from delegate into the view

--- files/app/application/application_delegate.rb
+++ files/app/application/application_delegate.rb
@@ -1,11 +1,8 @@
 class ApplicationDelegate
   include AppDelegateHelper
-  # Replace root_view_controller with your own
-  # Rubymotion displays warnings if it is not set
+
   def initialize
     # return true if RUBYMOTION_ENV == 'test'
     @window = ApplicationWindow.new
-    @window.root_view_controller = ExampleController.new
-    @window.make_key_and_visible
   end
 end
--- files/app/views/application/application_window.rb
+++ files/app/views/application/application_window.rb
@@ -1,7 +1,11 @@
 class ApplicationWindow < UIWindow
   include UIWindowHelper
 
+  # Replace root_view_controller with your own
+  # Rubymotion displays warnings if it is not set
   def initialize
     self.frame = DeviceScreen.main_screen.bounds
+    self.root_view_controller = ExampleController.new
+    self.make_key_and_visible
   end
 end
